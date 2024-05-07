e1a2eb224768712dc5de29d649ae5d7b006bc2d7
veefgiy <shw213967@email.com>
2024-05-07 18:48:30 +0900

    feature: 메서드 수정2

--- src/SHW/Sample.java
+++ src/SHW/Sample.java
@@ -11,7 +11,7 @@ public class Sample {
     }
 
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
         loop5();
     }
 
