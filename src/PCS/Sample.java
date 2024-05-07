a0359b741e2517d2ec89098819a18309e33892f9
pangseon <chaosen0521@gmail.com>
2024-05-07 19:02:30 +0900

    feature: 메서드 추가

--- src/PCS/Sample.java
+++ src/PCS/Sample.java
@@ -6,7 +6,9 @@ public class Sample {
     public static void main(String[] args) {
     
         System.out.printf("Hello and welcome!");
-
+        asdf();
+    }
+    public static void asdf(){
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
